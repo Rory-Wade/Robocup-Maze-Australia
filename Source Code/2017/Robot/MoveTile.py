cba3a0af86d0f07decc369b2e2683ca9ea1c4bde
Rory Wade Robot <rory@threatvectorx.com>
2017-04-12 02:23:01 +0000

    Basic Movement adjustments acording to Accel Heading

--- Source Code/2017/Robot/MoveTile.py
+++ Source Code/2017/Robot/MoveTile.py
@@ -18,6 +18,12 @@ socket.setsockopt_string(zmq.SUBSCRIBE, filter)
 global currentDirection
 currentDirection = 0
 
+KP = 3.5
+KI = 0.45
+Kd = 0.5
+
+baseMotorSpeed = 40
+
 def turn(currentAngle,toAngle):
     
     if currentAngle == None:
@@ -25,41 +31,34 @@ def turn(currentAngle,toAngle):
         return turn(getCurrentAngle(),toAngle)
     movingForward = False
     
-    #MoveMotors
-    #LEFT SIDE RIGHT SIDE
     if abs((currentAngle - toAngle) % 360) < 3:
         StopMotors()
         movingForward = True
         return True
     elif abs((currentAngle - toAngle) % 360) < 180:
-        #LEFT
-        #WHERE THE CODE GOES
+
         rightMotorSpeed = (abs(abs(currentAngle - toAngle) % 360) / 180) * 70 + 5
         leftMotorSpeed = -((abs(abs(currentAngle - toAngle) % 360) / 180) * 70) - 5
-        #print(leftMotorSpeed)
-        #print(rightMotorSpeed)
+
         MoveMotors(leftMotorSpeed,rightMotorSpeed)
     else:
-        #RIGHT
-        #WHERE THE CODE GOES
+
         leftMotorSpeed = (abs(abs(currentAngle - toAngle) % 360) / 180) * 70 + 5
         rightMotorSpeed = -((abs(abs(currentAngle - toAngle) % 360) / 180) * 70) - 5
-        #print(leftMotorSpeed)
-        #print(rightMotorSpeed)
-        #print(currentAngle, toAngle)
+
         MoveMotors(leftMotorSpeed,rightMotorSpeed)
     time.sleep(0.05)
     return turn(getCurrentAngle(),toAngle)
 
 def callMotors(direction):
-    #do something here?
+
     print("DIRECTION TO MOVE")
     print(direction)
     if direction == 1:
-        #TURN RIGHT
+        
         turn(getCurrentAngle(),90)
     elif direction == 3:
-        #TURN LEFT
+
         turn(getCurrentAngle(),270)
         
 def moveDirection(direction):
@@ -92,6 +91,20 @@ def MoveMotors(Linput,Rinput):
 def StopMotors():
     motors.send(b"%i,%i" % (0,0))
     message = motors.recv()
+
+def PID():
+    angle = getCurrentAngle()
+    
+    if currentDirection == 0:
+        if angle > 180:
+            angle = (360 - angle) * -1 
+        
+    else:
+        angle = ((90 * currentDirection)  - angle) * -1
+    
+    angle = angle * 2
+    
+    MoveMotors(baseMotorSpeed - angle,baseMotorSpeed + angle)  
     
 print("ONLINE")
 
@@ -100,7 +113,8 @@ while True:
 
     if lidarArray[0] > 200:
         #print("N: %s E: %s S: %s W: %s \n\n\n\n " % (lidarINPUT[0],lidarINPUT[9],lidarINPUT[18],lidarINPUT[27],))
-        MoveMotors(40,40)
+        #MoveMotors(40,40)
+        PID()
     else:
         MoveMotors(0,0)
         if currentDirection == 0:
