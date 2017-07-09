1ad4adab9e42879b2a9bdf096ac47064b8ceb293
Demo User <debian@beaglebone.localdomain>
2017-07-09 11:39:47 +0000

    Fix up serial test code

--- Source Code/2017/Robot/testSerial.py
+++ Source Code/2017/Robot/testSerial.py
@@ -1,8 +1,11 @@
 import serial
+import time
+# O1 = left
+# O4 = right
 
 port = serial.Serial(
     '/dev/ttyO1',
-    baudrate=115200,
+    baudrate=19200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS
@@ -10,6 +13,12 @@ port = serial.Serial(
 
 value = 0
 while True:
-    print port.readline()
-    value += 1
-    port.write("Hello from beaglebone! %i\n" % value)
+    cameraData = port.readline() 
+    print(cameraData)
+    if "H" in cameraData:
+        print("FOUND AN H")
+        port.reset_input_buffer()
+        port.write("D1")
+        time.sleep(4)
+        print(port.readline())
+        
